f97c7dec35bd6648e2c1c7b0bf04c15ffa543261
Oliver Tabay <otabay@zynga.com>
2016-07-29 16:35:37 -0400

    event changes

--- routes/views/events.js
+++ routes/views/events.js
@@ -5,6 +5,8 @@ exports = module.exports = function(req, res) {
 
 	var view = new keystone.View(req, res);
 	var locals = res.locals;
+	var eventsCategory='events';
+	var weeklySeries='wednesday-night';
 
 	// Init locals
 	locals.section = 'events';
@@ -12,40 +14,56 @@ exports = module.exports = function(req, res) {
 		category: req.params.category
 	};
 	locals.data = {
-		posts: [],
+		events: [],
+		weeklyEvents: [],
 		categories: []
+	};
+
+	function findCategory(categoryName) {
+		var l_categoryName = categoryName;
+		var l_category = null;
+		if (req.params.category) {
+			l_categoryName = locals.filters.category;
+		};
+		keystone.list('PostCategory').model.findOne({ key: l_categoryName }).exec(function(err, result) {
+			l_category = result;
+		});
+		return l_category;
+	};
+
+	function findCategoryPosts(categoryName){
+		var categoryPosts = [];
+		var q = keystone.list('Post').paginate({
+				page: req.query.page || 1,
+				perPage: 10,
+				maxPages: 10
+			})
+			.where('state', 'published')
+			.sort('-publishedDate')
+			.populate('author categories');
+
+		if (locals.data.category) {
+			q.where('categories').in([locals.data.eventsCategory]);
+		}
+
+		q.exec(function(err, results) {
+			categoryPosts = results;
+		});
+		return categoryPosts;
 	};
 
 	// Load the current category filter
 	view.on('init', function(next) {
-
-		var l_category = 'Events';
-		if (req.params.category) {
-			l_category = locals.filters.category;
-		}
-		keystone.list('PostCategory').model.findOne({ key: l_category }).exec(function(err, result) {
-			locals.data.category = result;
-			next(err);
-		});
+		locals.data.eventsCategory = findCategory(eventsCategory);
+		locals.data.events = findCategoryPosts(locals.data.eventsCategory);
+		next();
 
 	});
 
 	// Load the posts
 	view.on('init', function(next) {
 
-		var q = keystone.list('Post')
-			.where('state', 'published')
-			.sort('-publishedDate')
-			.populate('author categories');
 
-		if (locals.data.category) {
-			q.where('categories').in([locals.data.category]);
-		}
-
-		q.exec(function(err, results) {
-			locals.data.posts = results;
-			next(err);
-		});
 
 	});
 
